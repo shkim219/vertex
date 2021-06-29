16be1b4af4aa8e799f56a16efb24f66712a2dbda
shkim219 <shkim219@bu.edu>
2021-06-28 23:08:08 -0400

    Update command_retrieve.py

--- shkim219/commands/command_retrieve.py
+++ shkim219/commands/command_retrieve.py
@@ -2,8 +2,8 @@ import click
 import shkim219.query
 
 @click.command("retrieve")
-@click.argument('filename', nargs=1, type=click.Path)
-@click.option("--output", "-o", nargs=1, type=click.Path())
+@click.argument('filename', nargs=1, type=click.Path())
+@click.argument('output', nargs=1, type=click.Path())
 def command(filename, output):
     shkim219.query.createFile(filename, output)
 
