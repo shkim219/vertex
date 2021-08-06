7e8a126ffefc49fb0b9f6d89f0ed0694e1b38465
shkim219 <shkim219@bu.edu>
2021-08-06 12:27:25 -0400

    c

--- shkim219/commands/command_kmeans.py
+++ shkim219/commands/command_kmeans.py
@@ -10,7 +10,7 @@ def command(filename):
     print(curpath)
     cd_in = os.system("cd " + curpath + "\\vertex\\skopytest")
     cd_execute = os.system("mvn exec:java -Dexec.mainclass=\"kmeans.Client\" -Dexec.args=\"" + filename + " " + curpath + "\\vertex\\shkim219\\query\\__init__.py\"")
-    shkim219.query2.create_cell(curpath + "\\vertex\\skopytest\\" + filename[0,filename.index(".csv")],"kmeans")
+    shkim219.query2.create_cell(curpath + "\\vertex\\skopytest\\" + filename[0:filename.index(".csv")],"kmeans")
     
 
     
--- shkim219/commands/command_maven.py
+++ shkim219/commands/command_maven.py
@@ -7,8 +7,9 @@ def command():
     cd_back = os.system("cd ..")
     cd_back = os.system("cd ..")
     cd_install = os.system("wget https://mirrors.advancedhosters.com/apache//ignite/2.10.0/apache-ignite-2.10.0-bin.zip")
-    cd_unzip = os.system("tar -xvf apache-ignite-2.10.0-bin.zip")
-    cd_in = os.system("cd skopytest")
+    cd_unzip = os.system("unzip apache-ignite-2.10.0-bin.zip")
+    curpath = os.path.abspath(os.getcwd())
+    cd_in = os.system("cd " + curpath + "\\vertex\\skopytest")
     cd_install = os.system("mvn install")
     cd_compile = os.system("mvn compile")
     
