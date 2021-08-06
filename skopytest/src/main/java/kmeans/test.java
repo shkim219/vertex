073c494132074802f978aee60b1df9b892fc734d
shkim219 <shkim219@bu.edu>
2021-08-06 09:23:08 -0400

    Update test.java

--- skopytest/src/main/java/kmeans/test.java
+++ skopytest/src/main/java/kmeans/test.java
@@ -7,13 +7,7 @@ import java.nio.Buffer;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        String s = null;
-        Process p = Runtime.getRuntime().exec("python C:\\Users\\paulk\\PycharmProjects\\vertex-main\\vertex-main\\shkim219\\query2\\__init__.py");
-
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-        while ((s = stdInput.readLine()) != null){
-            System.out.println(s);
-        }
+        System.out.println("Hi");
+        System.out.println("testing");
     }
 }
