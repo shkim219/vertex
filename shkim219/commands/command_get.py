286f04f74968d2a0479c34d171482ad8b9c2d8d3
shkim219 <shkim219@bu.edu>
2021-08-10 13:32:05 -0400

    c

--- shkim219/commands/command_get.py
+++ shkim219/commands/command_get.py
@@ -4,10 +4,9 @@ import shkim219.query
 @click.command("get")
 @click.argument('filename', nargs=1, type=click.Path())
 def command(filename):
-    retArray = shkim219.retrievecells(filename)
+    retArray = shkim219.query.retrievecells(filename)
     for singleCell in retArray:
         print(singleCell)
 
 if __name__ == '__main__':
     command()
-
